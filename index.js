1f5401c61fedcc135c8cef1cb716846b3960a093
Manilo4ka <marinad.88@mail.ru>
2022-04-12 23:15:48 +0300

    task2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 var app = express();
 app.listen(8080);
-app.get('/name/:name', function(req, res) {
+
+app.get('/name:name', function(req, res) {
+//console.log(req.query);
 	res.send('Guten Tag, Frau ' + req.params.name);
 });
+
 app.get('/', function(req, res) {
+  console.log('Name: '+ req.query['name']);
+    console.log('Age: '+ req.query['age']);
 	res.send('Guten Tag');
 });
   //comment

3fef7b2c6bd09666cfe22eed03a27c5096a15fef
Manilo4ka <marinad.88@mail.ru>
2022-04-14 14:45:47 +0300

    3 задание

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
-app.listen(8080);
+app.listen(80);
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.get('/name:name', function(req, res) {
 //console.log(req.query);
 	res.send('Guten Tag, Frau ' + req.params.name);
 });
 
-app.get('/', function(req, res) {
-  console.log('Name: '+ req.query['name']);
-    console.log('Age: '+ req.query['age']);
-	res.send('Guten Tag');
+app.get('/login', function(req, res) {
+//console.log(req.query);
+	res.sendFile(__dirname + "/static/login.html");
 });
-  //comment
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/static/index.html");
+});
+
+app.post('/login', urlencodedParser, function(req,res) {
+	if (!req.body) return res.sendStatus(400);
+ //console.log(req.body);
+ console.log('Name: '+ req.body.new_email);
+ console.log('Pass: '+ req.body.new_pass);
+ res.sendFile(__dirname + "/static/login.html");
+});
